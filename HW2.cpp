ce7527e14ae959609fbe8552bdc2b6904cc057d0
jvm3487 <jvm3487@gmail.com>
2015-09-13 16:50:22 -0400

    Updated with parsing the input.

--- HW2.cpp
+++ HW2.cpp
@@ -3,9 +3,72 @@
 #include <cstdlib>
 #include <map>
 #include <vector>
-#include <tuple>
 #include <sstream>
+#include <string>
+#include <climits>
 #include <string.h>
+
+struct _otherData
+{
+  std::string nextHop;
+  std::vector<std::string> asPath;
+  bool bIGP;
+  int MED;
+  _otherData(): bIGP(false), MED(INT_MAX) {}
+};
+typedef struct _otherData otherData;
+
+void parseInput(const std::string& line, std::vector<std::string>& changeIP, otherData& oneBlockData, bool& announceType)
+{
+
+  // Gather the Next Hop IP Address
+  if (line.find("FROM:") != std::string::npos)
+    {
+      std::stringstream destIP;	  
+      for (unsigned strIndex = 6; line[strIndex] != ' '; strIndex++)
+	destIP << line[strIndex];
+      oneBlockData.nextHop = destIP.str();
+    }
+      
+  // Gather the origin information
+  else if (line.find("ORIGIN:") != std::string::npos)
+    oneBlockData.bIGP = (line.find("IGP") != std::string::npos);
+
+  // Gather the ASPATH
+  else if (line.find("ASPATH:") != std::string::npos)
+    {
+      for (unsigned currentIndex = 8 ; currentIndex - 1 < line.size() ; currentIndex++)
+	{
+	  std::stringstream oneASpath;
+	  for ( ; line[currentIndex] != ' ' && line[currentIndex] != '\n'; currentIndex++)
+	    oneASpath << line[currentIndex];
+	  oneBlockData.asPath.push_back(oneASpath.str());
+	}
+    } 
+  
+  // Gather the MED
+  else if (line.find("MULTI_EXIT_DISC:") != std::string::npos)
+    {
+      std::stringstream medStream;
+      for (unsigned strIndex = 17; line[strIndex] != ' '; strIndex++)
+	medStream << line[strIndex];
+      oneBlockData.MED = std::stoi(medStream.str());
+    }
+
+  // Determine if announce
+  else if (line.find("ANNOUNCE") !=std::string::npos)
+    announceType = true;
+  
+  // Determine announced ip addresses
+  else if (line[0] == ' ')
+    {
+      std::stringstream ipAdd;
+      for (unsigned currentIndex = 2 ; currentIndex - 1 < line.size(); currentIndex++)
+	ipAdd << line[currentIndex];
+      changeIP.push_back(ipAdd.str());
+    }
+  
+}
 
 int main (int argc, char* argv[])
 {
@@ -50,27 +113,36 @@ int main (int argc, char* argv[])
     }
 
   // IP Address map to
-  // Next Hop IP Address, AS path, origin type IDP=1, and MED
-  std::map<std::string, std::tuple<std::string, std::vector<int>, bool, int> > mapIPAddress;
+  // Next Hop IP Address, AS path, origin type IDP=true, and MED
+  std::map<std::string, otherData > mapIPAddress;
 
   std::string line;
-  std::stringstream destIP;
-  std::tuple<std::string, std::vector<int>, bool, int> otherInfo;
-  while (std::getline(inputFile, line))
-    {
-      std::cout << line << std::endl;
-      // Gather the Next Hop IP Address
-      if (line.find("FROM:") != std::string::npos)
-	{
-	  for (int strIndex = 6; line[strIndex] != ' '; strIndex++)
-	  destIP << line[strIndex];
-	}
-      if (line.empty())
-	{
-	  std::cout << destIP.str() << std::endl;
-	  break;
-	}
-    }
+ 
+  while (!inputFile.eof()){
+  
+    std::vector<std::string> changeIP;
+    otherData oneBlockData;
+    bool announceType = false; //true if announce, false otherwise
+
+    while (std::getline(inputFile, line))
+      {
+      	
+	if (line.empty() || inputFile.eof())
+	  {
+	    std::cout << oneBlockData.nextHop << std::endl;
+	    for (unsigned i = 0; i < oneBlockData.asPath.size(); i++)
+	      std::cout << oneBlockData.asPath[i] << std::endl;
+	    std::cout << oneBlockData.bIGP << std::endl;
+	    std::cout << oneBlockData.MED << std::endl;
+	    for (unsigned i = 0; i < changeIP.size(); i++)
+	      std::cout << changeIP[i] << std::endl;
+	    std::cout << announceType << std::endl;
+	    break;
+	  }
+	else
+	  parseInput(line, changeIP, oneBlockData, announceType);
+      }
+  }
 
   inputFile.close();
   return EXIT_SUCCESS;
--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 HW2: HW2.cpp
-	g++ -std=c++11 -pedantic -Werror -Wall -o $@ $<
+	g++ -ggdb -std=c++11 -pedantic -Werror -Wall -o $@ $<
