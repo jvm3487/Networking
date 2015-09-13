0720d1836934619d40fda06b7f6318002a7cffa8
jvm3487 <jvm3487@gmail.com>
2015-09-13 18:25:39 -0400

    Part 1 should be done I think...

--- HW2.cpp
+++ HW2.cpp
@@ -70,6 +70,74 @@ void parseInput(const std::string& line, std::vector<std::string>& changeIP, oth
   
 }
 
+void updateMapWithNewInfo(std::map<std::string, otherData>& mapIPAddress, const std::string& changeIPStr, const otherData& oneBlockData, const bool announceType)
+{
+  std::map<std::string, otherData>::iterator it;
+  it = mapIPAddress.find(changeIPStr);
+  
+  if (announceType)
+    {
+      // check to see if the information should be updated or left as is
+      if (it != mapIPAddress.end())
+	{
+	  // first check - AS path length
+	  if (it->second.asPath.size() < oneBlockData.asPath.size())
+	    return;
+	  else if (it->second.asPath.size() == oneBlockData.asPath.size()) //same - go to next tiebreaker
+	    {
+	      // second check - better not IGP
+	      if (it->second.bIGP == false && oneBlockData.bIGP == true)
+		return;
+	      else if (it->second.bIGP == oneBlockData.bIGP) // same - go to next tiebreaker
+		{
+		  // third check - MED
+		  if (it->second.MED < oneBlockData.MED)
+		    return;
+		  else if (it->second.MED == oneBlockData.MED) // same - go to final tiebreak
+		    {
+		      unsigned currentIndexMap = 0;
+		      unsigned currentIndexChallenger = 0;
+		      unsigned firstTotal = 0;
+		      unsigned secondTotal = 0;
+		      while ( currentIndexMap - 1 < it->second.nextHop.size())
+			{
+			  std::stringstream ipStream;
+			  std::stringstream ipStream2;
+			  for ( ; it->second.nextHop[currentIndexMap] != '.' && it->second.nextHop[currentIndexMap] != ':' && currentIndexMap - 1 < it->second.nextHop.size(); currentIndexMap++)
+			    ipStream << it->second.nextHop[currentIndexMap];
+			  firstTotal = 0;
+			  if (!ipStream.rdbuf()->in_avail())
+			    firstTotal = std::stoi(ipStream.str());
+			  for ( ; oneBlockData.nextHop[currentIndexChallenger] != '.' && oneBlockData.nextHop[currentIndexChallenger] != ':' && currentIndexChallenger - 1 < oneBlockData.nextHop.size(); currentIndexChallenger++)
+			    ipStream2 << it->second.nextHop[currentIndexMap];
+			  secondTotal = 0;
+			  if (!ipStream2.rdbuf()->in_avail())
+			    secondTotal = std::stoi(ipStream2.str());
+			  if (firstTotal != secondTotal)
+			    break;
+			  currentIndexChallenger++;
+			  currentIndexMap++;
+			}
+		      if (firstTotal <= secondTotal)
+			return;
+		    }
+		  
+		}
+	    }
+	  it->second = oneBlockData;
+	}
+      else
+	{
+	  mapIPAddress.emplace(changeIPStr, oneBlockData);
+	}
+    }
+  else
+    {
+      if (it != mapIPAddress.end())
+	mapIPAddress.erase(it);
+    }
+}
+
 int main (int argc, char* argv[])
 {
   bool inputFailure = true;
@@ -129,21 +197,33 @@ int main (int argc, char* argv[])
       	
 	if (line.empty() || inputFile.eof())
 	  {
-	    std::cout << oneBlockData.nextHop << std::endl;
-	    for (unsigned i = 0; i < oneBlockData.asPath.size(); i++)
-	      std::cout << oneBlockData.asPath[i] << std::endl;
-	    std::cout << oneBlockData.bIGP << std::endl;
-	    std::cout << oneBlockData.MED << std::endl;
-	    for (unsigned i = 0; i < changeIP.size(); i++)
-	      std::cout << changeIP[i] << std::endl;
-	    std::cout << announceType << std::endl;
 	    break;
 	  }
 	else
-	  parseInput(line, changeIP, oneBlockData, announceType);
+	  {
+	    parseInput(line, changeIP, oneBlockData, announceType);
+	  }
+      }
+
+    // update the map with the changed IP address
+    for (unsigned i = 0; i < changeIP.size(); i++)
+      {
+	updateMapWithNewInfo(mapIPAddress, changeIP[i], oneBlockData, announceType);
       }
   }
 
+  // print out the map in the correct format
+  for (const auto& mapValue : mapIPAddress)
+    {
+      std::cout << mapValue.first << " " << mapValue.second.nextHop;
+      for (const auto& asPathValue : mapValue.second.asPath)
+	{
+	  std::cout << " " << asPathValue; 
+	}
+      std::cout << std::endl;
+    } 
+
   inputFile.close();
+
   return EXIT_SUCCESS;
 }
